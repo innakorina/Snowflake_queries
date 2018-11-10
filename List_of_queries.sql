62de81308dba7bc9c0427ed7821c7b39beb12eb9
CynCo <cynco8d@gmail.com>
2018-11-09 19:40:45 -0500

    Update List_of_queries.sql

--- List_of_queries.sql
+++ List_of_queries.sql
@@ -16,8 +16,8 @@ s   = reservation_bookreservationstatus
 Queries:
 
 1. 
-count of reservations by active users
-active users = r.date_created < [90 days]
+"Count of reservations by active users
+active users = r.date_created < [90 days]"
 
 select  r1.user_id active_user, 
         count(r1.id) Total_Reservations, 
@@ -30,5 +30,15 @@ where r1.cancellation_id is null
 (select r2.user_id from resy_info r2 where r2.date_created between '2018-08-02' and '2018-10-31')
 group by 1;
 
+2.
+"Reference table of location ids, city names, number of venues, and number of reservations"
 
-hi from Inna
+select li.id "location_info.id", li.name "location_info.name", count(distinct v.id) "venue_info.id", count(r.id) "res..bookres..id"
+from reservation_bookreservation r 
+join venue_info v on v.id=r.venue_id and v.is_active=1
+inner join location_info li on li.id=v.location_id
+group by li.id, li.name
+order by li.id
+;
+// "had to add li.name to group by to be able to select li.name"
+
