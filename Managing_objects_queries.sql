1a401cd27c02e2b87302f26cd89d3555b291ce8e
CynCo <cynco8d@gmail.com>
2018-11-19 14:50:47 -0500

    some create table commands

---
+++ Managing_objects_queries.sql
@@ -0,0 +1,16 @@
+Description: Sample queries to manage objects such as creating tables, etc.
+
+use database "PC_FIVETRAN_DB" ;
+use schema "AURORA_CORE";
+
+create table "user_locations_by_max" clone "EXPERIMENTAL"."PUBLIC"."USER_LOCATIONS_BY_MAX";
+
+
+clone "EXPERIMENTAL"."PUBLIC"."USER_LOCATIONS_BY_MAX" into table ;
+--- need to verify and fix
+
+// rerun the following any time a table is added to the schema
+grant select on all tables in schema "PC_FIVETRAN_DB"."AURORA_CORE" to role looker_role;
+
+// change ownership of looker_scratch table or schema to pc_fivetran_role
+grant ownership on schema looker_scratch to role pc_fivetran_role REVOKE CURRENT GRANTS;
