4127dbd2bbe11e96e357a278b39b2806198bebfc
Inna Korzhovska <ikorzhovska@gradcenter.cuny.edu>
2018-12-18 10:53:37 -0500

    remove table upload
    remove table upload

--- Snowflake_tutorial.sql
+++ Snowflake_tutorial.sql
@@ -52,7 +52,7 @@ alter table user_locations alter user_ID set data type NUMBER(20,0);
 copy t (x1, ... , x10) from '/path/to/my_file' with (format csv)
 
 
-4. Save query as a new table
+5. Save query as a new table
 
 CREATE TABLE experimental.public.active_users2 (User_ID NUMBER(20,0), resycount NUMBER(20,0)) 
 AS
@@ -65,24 +65,8 @@ GROUP BY u.id
 order by u.id
 ;
 
-Warnings!: make sure the header is removed, there is no NAs in the data, and the csv format has the same number of the columns (the filewriting function doesn’t create a count column) 
 
-
-5. Uploading the data from the local computer
-In Terminal type: 
-Innas-MacBook-Pro:Documents innakorzhouska$ alias snowsql=/Applications/SnowSQL.app/Contents/MacOS/snowsql
---not needed in Windows
-Innas-MacBook-Pro:Documents innakorzhouska$ snowsql
-Innas-MacBook-Pro:Documents innakorzhouska$ snowsql -a yk58234.us-east-1 -u INNA
-INNA#(no warehouse)@(no database).(no schema)>use database experimental;   (important to put “;”)
-INNA#(no warehouse)@EXPERIMENTAL.PUBLIC>put file:///Users/innakorzhouska/Documents/user_locations.csv @"PC_FIVETRAN_DB”.”PUBLIC”."EXP_STAGE";
----------then in the Snowflake create a table and stage the file into new table
-create table PC_FIVETRAN_DB.AURORA_CORE.user_location (User_ID NUMBER(20,0), Loc_primary NUMBER(20,0), Count_primary NUMBER(20,0), weekends NUMBER(20,0), weekdays NUMBER(20,0), score_primary NUMBER(20,0), loc_secondary NUMBER(20,0), Count_secondary NUMBER(20,0), score_secondary NUMBER(20,0));
-COPY INTO PC_FIVETRAN_DB.AURORA_CORE.user_location from '@PC_FIVETRAN_DB.PUBLIC.EXP_STAGE/user_locations.csv.gz' file_format = (compression ='gzip');
-
-
-
-       6. Select
+6. Select
 https://docs.snowflake.net/manuals/sql-reference/sql/select.html
 Ex:
 
@@ -138,6 +122,7 @@ select i, count(*), count(j) from count_example group by i;
 |   12 |        1 |        1 |
 | NULL |        2 |        2 |
 +------+----------+----------+
+
 8.Dense_Rank ranking the values from certain field
 
 select state, bushels_produced, dense_rank()
@@ -169,8 +154,6 @@ select state, bushels_produced, dense_rank()
 https://docs.snowflake.net/manuals/sql-reference/functions/dense_rank.html
 
 9. Case
-
-
 https://docs.snowflake.net/manuals/sql-reference/functions/case.html
 
 select
